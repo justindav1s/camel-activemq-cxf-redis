0c0e378e7d01d17a27c54b2def867296631f3ef0
Justin Davis <justin.davis@ba.com>
2014-07-11 20:47:57 +0100

    changes

---
+++ selling-routes/src/main/java/com/ba/captwo/eda/demo/resources/PersonServiceResource.java
@@ -0,0 +1,56 @@
+package com.ba.captwo.eda.demo.resources;
+
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+/**
+ * Created by u760245 on 05/07/2014.
+ */
+@Component("PersonServiceResource")
+@Path("/person")
+public class PersonServiceResource {
+
+
+    @GET
+    @Produces("application/json")
+    @Consumes("application/json")
+    @Path("/create")
+    public Response createPerson(
+            @MatrixParam("fname") String fname,
+            @MatrixParam("lname") String lname,
+            @MatrixParam("address") String address,
+            @MatrixParam("city") String city){return null;}
+
+    @GET
+    @Produces("application/json")
+    @Consumes("application/json")
+    @Path("/read")
+    public Response readPerson(
+            @MatrixParam("pid") int pid){return null;};
+
+    @GET
+    @Produces("application/json")
+    @Consumes("application/json")
+    @Path("/update")
+    public Response updatePerson(
+            @MatrixParam("pid") int pid,
+            @MatrixParam("fname") String fname,
+            @MatrixParam("lname") String lname,
+            @MatrixParam("address") String address,
+            @MatrixParam("city") String city){return null;};
+
+    @GET
+    @Produces("application/json")
+    @Consumes("application/json")
+    @Path("/delete")
+    public Response deletePerson(
+            @MatrixParam("pid") int pid){return null;};
+
+    @GET
+    @Produces("application/json")
+    @Consumes("application/json")
+    @Path("/list")
+    public Response listPersons(){return null;};
+}
