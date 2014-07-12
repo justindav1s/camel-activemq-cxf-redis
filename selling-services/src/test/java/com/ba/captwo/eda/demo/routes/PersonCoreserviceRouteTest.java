edd0a3146597497a5a9764a5e4939a96f3325a9e
Justin Davis <justin.davis@ba.com>
2014-07-12 23:25:51 +0100

    changes

--- selling-services/src/test/java/com/ba/captwo/eda/demo/routes/PersonCoreserviceRouteTest.java
+++ selling-services/src/test/java/com/ba/captwo/eda/demo/routes/PersonCoreserviceRouteTest.java
@@ -1,12 +1,9 @@
 package com.ba.captwo.eda.demo.routes;
 
-import com.ba.captwo.eda.demo.model.Booking;
 import com.ba.captwo.eda.demo.model.Person;
 import org.apache.camel.CamelContext;
 import org.apache.camel.EndpointInject;
-import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
-import org.apache.camel.builder.ValueBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.test.spring.CamelSpringJUnit4ClassRunner;
 import org.apache.camel.test.spring.DisableJmx;
@@ -23,33 +20,37 @@ import org.springframework.test.context.ContextConfiguration;
  * Created by u760245 on 12/07/2014.
  */
 @RunWith(CamelSpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "/beans_remote_db.xml" })
+@ContextConfiguration(locations = { "/camel_beans.xml" })
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @MockEndpoints("log:*")
 @DisableJmx(false)
 
 
-public class PersonCoreserviceRoute2IT {
+public class PersonCoreserviceRouteTest {
 
-    private final static Logger log = LoggerFactory.getLogger(PersonCoreserviceRoute2IT.class);
+    private final static Logger log = LoggerFactory.getLogger(PersonCoreserviceRouteTest.class);
 
     @Autowired
     protected CamelContext camelContext;
 
     @EndpointInject(uri = "mock:log:output")
-    protected MockEndpoint mockLog;
+    protected MockEndpoint mockEndPoint;
 
     @Test
     public void testPositive() throws Exception {
 
-        log.info("****** camelContext : "+ camelContext);
-        mockLog.setExpectedMessageCount(1);
-        mockLog.message(0).body().isNotNull();
-        mockLog.message(0).header("pid").isEqualTo(1921);
-        ValueBuilder vb = mockLog.message(0).body().convertTo(Booking.class);
+        int pid = 1921;
+
+        Person p = new Person();
+        p.setPersonID(pid);
+
+        mockEndPoint.setExpectedMessageCount(1);
+        mockEndPoint.message(0).body().isNotNull();
+        mockEndPoint.message(0).header("pid").isEqualTo(pid);
+        mockEndPoint.message(0).body().isEqualTo(p);
 
         ProducerTemplate t = camelContext.createProducerTemplate();
-        t.sendBodyAndHeader("direct-vm:PersonCoreService", 1921, "pid", 1921);
+        t.sendBodyAndHeader("direct-vm:PersonCoreService", pid, "pid", pid);
         MockEndpoint.assertIsSatisfied(camelContext);
     }
 }
