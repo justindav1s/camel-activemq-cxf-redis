454d030909097453a0945403ef25108f9c2bef5c
Justin Davis <justin.davis@ba.com>
2014-07-17 14:58:00 +0100

    added docs

--- app.sql
+++ app.sql
@@ -1,3 +1,13 @@
+create an Apache Derby database called "ba"
+
+using ij, like so :
+
+ connect 'jdbc:derby:ba;create=true';
+
+then :
+
+
+
 CREATE SEQUENCE BA_SEQ AS INT MINVALUE 1000 MAXVALUE 999999 CYCLE;
 CREATE TABLE persons(PersonID int,lastName varchar(255),firstName varchar(255),address varchar(255),city varchar(255), PRIMARY KEY (PersonID));
 CREATE TABLE bookings (personID int,bookingId int,flightnum varchar(255),tickets int,cabin  varchar(255), PRIMARY KEY (bookingId));
